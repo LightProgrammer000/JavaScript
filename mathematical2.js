b4aa1986057c2d190b2aeac606396bf05ad30888
LightProgrammer000 <113267077+LightProgrammer000@users.noreply.github.com>
2022-09-11 09:59:13 -0300

    Create mathematical2.js
    Matemática [Classe e Objeto]

---
+++ mathematical2.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<!-- Cabeçalho -->
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<title> Testando JavaScript </title>
+
+		<!-- Seção Css -->
+        <style> </style>
+        
+        <!-- Seção JavaScript -->
+		<script type="text/javascript">
+
+			document.write(" - Logaritmo Neperiano [2]: " + Math.LN2 + "<br>");
+			document.write(" - Logaritmo Neperiano [10]: " + Math.LN10 + "<br> <br>");
+
+			document.write(" - Valor de PI: " + Math.PI + "<br>");
+			document.write(" - Valor absoluto: " + Math.abs(-1) + "<br>");
+			document.write(" - Raiz Quadrada de [2]: " + Math.SQRT2 + "<br>");
+			document.write(" - Raiz Quadrada de [1/2]: " + Math.SQRT1_2 + "<br> <br>");
+
+			document.write(" - Seno [45]: " + Math.sin(30/57.2958) + "<br>");
+			document.write(" - Cosseno [30]: " + Math.cos(30/57.2958) + "<br>");
+			document.write(" - Tangente [30]: " + Math.tan(45/57.2958) + "<br> <br>");
+
+			document.write(" - Aleatório: " + Math.random() + "<br>");
+			document.write(" - Comparação [menor: 10,5,2,39]: " + Math.min(10,5,2,39) + "<br>");
+			document.write(" - Comparação [maior: 10,5,2,39]: " + Math.max(10,5,2,39) + "<br>");
+			document.write(" - Arredondamento (57.2958) [cima]: " + Math.ceil(57.2958) + "<br>");
+			document.write(" - Arredondamento (57.2958) [baixo]: " + Math.floor(57.2958) + "<br>");
+			document.write(" - Arredondamento (57.5000) [inteiro mais próximo]: " + Math.round(57.5000) + "<br>");
+
+        </script> 
+	</head>
+
+<!-- Corpo -->
+<body>
+
+</body>
+
+</html>
