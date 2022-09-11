707c2461bb5442aca52acf4341ca7235dd090135
LightProgrammer000 <113267077+LightProgrammer000@users.noreply.github.com>
2022-09-10 23:44:25 -0300

    Create settings.js
    Definições

---
+++ settings.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<title> Testando JavaScript </title>
+        
+        <style> </style>
+        
+		<script type="text/javascript">
+		
+		function funcao()
+		{
+			var a = 10;
+			var b = 4.50;
+			var c = "Água";
+			var d = true;
+			var e;
+			
+			document.write(" - Variável: " + a + "<br> Tipo: " + typeof(a) + "<br>");
+			document.write("<br> <br> - Variável: " + b + "<br> Tipo: " + typeof(b) + "<br>");
+			document.write("<br> <br> - Variável: " + c + "<br> Tipo: " + typeof(c) + "<br>");
+			document.write("<br> <br> - Variável: " + d + "<br> Tipo: " + typeof(d) + "<br>");
+			document.write("<br> <br> - Variável: " + e + "<br> Tipo: " + typeof(e) + "<br>");				
+		}
+		
+		funcao();
+        
+        </script>
+	</head>
+
+<body>
+
+</body>
+
+</html>
