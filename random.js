2af1bd087be0f28eb9c539f05e1b86a90250cd6d
LightProgrammer000 <113267077+LightProgrammer000@users.noreply.github.com>
2022-09-11 10:00:02 -0300

    Create random.js
    Mega - Sena

---
+++ random.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<title> Testando JavaScript </title>
+
+        <style> </style>
+
+		<script type="text/javascript">
+			
+			document.write(" - Número [1]: " + Math.floor(Math.random() * 60 + 1) + "<br>");
+			document.write(" - Número [2]: " + Math.floor(Math.random() * 60 + 1) + "<br>");
+			document.write(" - Número [3]: " + Math.floor(Math.random() * 60 + 1) + "<br>");
+			document.write(" - Número [4]: " + Math.floor(Math.random() * 60 + 1) + "<br>");
+			document.write(" - Número [5]: " + Math.floor(Math.random() * 60 + 1) + "<br>");
+			document.write(" - Número [6]: " + Math.floor(Math.random() * 60 + 1) + "<br>");		
+				
+        </script> 
+	</head>
+
+<body>
+
+</body>
+
+</html>
